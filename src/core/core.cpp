4354a62b4c6fc0763146bbdd3c9e9b4afe26a0bd
PanteR <panter.dsd@gmail.com>
2011-07-05 15:41:35 +0400

    Core: replace size / 1024 -> size >> 10

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -23,12 +23,12 @@ QString stringSize (qint64 size)
 										 QObject::tr ("GB")
 										};
 	static const size_t count = sizeof (postfixes) / sizeof (QString);
-	const qint64 tempSize = size / 1024;
+	const qint64 tempSize = size >> 10; //size >> 10 == size / 1024
 
 	size_t i = 0;
 	qint64 size_ = 1;
 
-	for (; i < count && tempSize >= size_; ++i, size_ *= 1024) {}
+	for (; i < count && tempSize >= size_; ++i, size_ = size_ << 10) {}//size << 10 == size * 1024
 
 	return size >= 0
 		   ? QString::number (static_cast <double> (size) / size_, 'f', 2) + " " + postfixes [i]
