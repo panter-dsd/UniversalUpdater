d4e4683728840b47f7e62d3b79642ed4580dbb19
PanteR <panter.dsd@gmail.com>
2011-07-06 11:40:09 +0400

    Utils::WidgetStateSettings is added.

---
+++ src/utils/widgetstatesettings.cpp
@@ -0,0 +1,79 @@
+#include <QtCore/QSettings>
+
+#include <QtGui/QWidget>
+
+#include "widgetstatesettings.h"
+
+class SettingsGroupChanger
+{
+public:
+	SettingsGroupChanger (QSettings *settings, const QString &group)
+		: settings_ (settings), group_ (group) {
+		assert (settings);
+
+		currentGroup_ = settings->group ();
+		cleanGroup ();
+		settings->beginGroup (group_);
+	}
+
+	~SettingsGroupChanger () {
+		cleanGroup ();
+		settings_->beginGroup (currentGroup_);
+	}
+
+private:
+	void cleanGroup () {
+		while (!settings_->group ().isEmpty()) {
+			settings_->endGroup ();
+		}
+	}
+
+private:
+	QSettings *settings_;
+	QString group_;
+	QString currentGroup_;
+};
+
+namespace Utils
+{
+
+void WidgetStateSettings::load (QSettings *settings)
+{
+	setQSettings (settings);
+	assert (settings_);
+
+	SettingsGroupChanger groupChanger (settings_, group_);
+
+	const QPoint pos = settings_->value ("pos").toPoint();
+
+	if (!pos.isNull()) {
+		widget_->move (pos);
+	}
+
+	const QSize size = settings_->value ("size", QSize (640, 480)).toSize();
+
+	widget_->resize (size);
+
+	const bool isMaximized = settings_->value ("IsMaximized", false).toBool();
+
+	if (isMaximized) {
+		widget_->setWindowState (Qt::WindowMaximized);
+	}
+}
+
+void WidgetStateSettings::save (QSettings *settings)
+{
+	setQSettings (settings);
+	assert (settings_);
+
+	SettingsGroupChanger groupChanger (settings_, group_);
+
+	if (widget_->windowState() != Qt::WindowMaximized) {
+		settings_->setValue ("pos", widget_->pos());
+		settings_->setValue ("size", widget_->size());
+		settings_->setValue ("IsMaximized", false);
+	} else {
+		settings_->setValue ("IsMaximized", true);
+	}
+}
+}
---
+++ src/utils/widgetstatesettings.h
@@ -0,0 +1,50 @@
+#ifndef WIDGETSTATESETTINGS_H
+#define WIDGETSTATESETTINGS_H
+
+#include <QtCore/QObject>
+
+#include <assert.h>
+
+class QWidget;
+class QSettings;
+
+namespace Utils
+{
+class WidgetStateSettings
+{
+
+public:
+	explicit WidgetStateSettings (QWidget *widget, QSettings *settings = 0)
+		: widget_ (widget), settings_ (settings),
+		  group_ ("GUI/MainWindow") {
+		assert (widget_);
+	}
+
+	void load (QSettings *settings = 0);
+	void save (QSettings *settings = 0);
+
+	QString group () const {
+		return group_;
+	}
+
+	void setGroup (const QString &group) {
+		group_ = group;
+	}
+
+	void setQSettings (QSettings *settings) {
+		if (settings) {
+			settings_ = settings;
+		}
+	}
+
+private:
+	WidgetStateSettings (const WidgetStateSettings &other);
+	WidgetStateSettings &operator= (const WidgetStateSettings &other);
+
+private:
+	QWidget *widget_;
+	QSettings *settings_;
+	QString group_;
+};
+}
+#endif // WIDGETSTATESETTINGS_H
